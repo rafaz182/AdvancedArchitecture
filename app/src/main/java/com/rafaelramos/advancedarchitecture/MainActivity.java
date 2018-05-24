97bd6ecf33636547552c69cd2b7e363e5688dab2
Rafael Pereira Ramos <raha148@hotmail.com>
2018-05-23 23:34:44 -0300

    Project creation

---
+++ app/src/main/java/com/rafaelramos/advancedarchitecture/MainActivity.java
@@ -0,0 +1,8 @@
+package com.rafaelramos.advancedarchitecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+}
